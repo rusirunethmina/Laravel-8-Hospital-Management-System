5673018f6d200e50113f38c3eecfbba4e91abf05
rusirunethmina <rusiruc21@gmail.com>
2021-12-29 20:08:12 +0530

    view change

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,3 @@
 <x-app-layout>
- <h1>this is admin dashboard</h1>
+ <h1>This is admin dashboard</h1>
 </x-app-layout>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,3 +1,3 @@
 <x-app-layout>
-    <h1>this is user dashboard</h1>
+    <h1>This is user dashboard</h1>
    </x-app-layout>
